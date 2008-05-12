e78d7b74a2d2a152ad4bec4172320ca70876337a
Ricardo SIGNES <rjbs@cpan.org>
2008-05-12 17:02:18 -0400

    docs

--- lib/MIME/ContentRewriter.pm
+++ lib/MIME/ContentRewriter.pm
@@ -7,6 +7,54 @@ use Encode;
 use MIME::Entity;
 use Variable::Magic ();
 
+=head1 NAME
+
+MIME::ContentRewriter - rewrite the content of text parts, minding charset
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+  MIME::ContentRewriter->rewrite_content(
+    $mime_entity,
+    sub {
+      my ($body_ref) = @_;
+      $$body_ref =~ s/zig/zag/;
+    },
+  );
+
+=head1 DESCRIPTION
+
+MIME::ContentRewriter provides methods for rewriting text parts.  These methods
+take care of character sets for you so that you can treat everything like text
+instead of worrying about content transfer encoding or character set encoding.
+
+At present, only MIME::Entity messages can be handled.  Other types will be
+added in the future.
+
+=head1 METHODS
+
+=head2 rewrite_content
+
+  MIME::ContentRewriter->rewrite_content($message, sub { ... });
+
+This method uses the given callback to rewrite the content (body) of the
+message.  It decodes the content (using Content-Transfer-Encoding and the
+Content-Type charset (or ISO-8859-1, if none is given)) and provides a
+reference to the character string to the coderef.  If the content is altered,
+the body will be re-encoded into the original charset and the message will be
+updated.
+
+The callback is invoked like this:
+
+  $code->(\$content, $message);
+
+In the future, there should be an option to re-encode to an alternate charset.
+
+=cut
+
 sub rewrite_content {
   my ($self, $entity, $code) = @_;
 
@@ -28,6 +76,20 @@ sub rewrite_content {
     $io->print(Encode::encode($charset, $body));
   }
 }
+
+=head2 rewrite_lines
+
+  MIME::ContentRewriter->rewrite_lines($message, sub { ... });
+
+This method behaves like C<rewrite_content), but the callback is invoked once
+per line, like this:
+
+  local $_ = $line;
+  $code->($message);
+
+If any line is changed, the entire body will be reencoded and updated.
+
+=cut
 
 sub rewrite_lines {
   my ($self, $entity, $code) = @_;
@@ -56,5 +118,27 @@ sub rewrite_lines {
     $io->print(Encode::encode($charset, $_)) for @lines;
   }
 }
+
+=head1 AUTHOR
+
+Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+
+=head1 THANKS
+
+Thanks to Pobox.com and Listbox.com, who sponsored the development of this
+module.
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>. I will be notified, and then you'll automatically be
+notified of progress on your bug as I make changes.
+
+=head1 COPYRIGHT
+
+Copyright 2008, Ricardo SIGNES.  This program is free software;  you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
 
 1;
